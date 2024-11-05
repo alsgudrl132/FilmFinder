12a07aaa121aea3dece640ba4231dde21919cbd9
alsgudrl133 <alsgudrl133@ebsap.co.kr>
2024-11-05 17:56:43 +0900

    Updates

---
+++ _nuxt/static/1730796122/movie/search/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/movie/search", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1730796122/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFilmFinder\u002F_nuxt\u002Fstatic\u002F1730796122",layout:"default",error:a,state:{state:{title:""}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FFilmFinder\u002F",assetsPath:"\u002FFilmFinder\u002F_nuxt\u002F",cdnURL:a}}}}(null));
